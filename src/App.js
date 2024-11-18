6893fdc683b7fb1e60f9006d050d656f09ae84d8
Shuvam_Pargal <Shuvam_Pargal@EPINHYDW01AC>
2024-11-18 15:58:15 +0530

    commit 1 for module 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Counter from './Counter';
+import SearchForm from './SearchForm';
+import GenreSelect from './Genres';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchQuery: '',
+			selectedGenre: '',
+			genreList: ['All', 'Action', 'Comedy', 'Drama', 'Romance', 'Sci-Fi'],  // List of genres
+		};
+	}
+
+	handleSearch = (query) => {
+		this.setState({ searchQuery: query });
+	};
+
+	handleSelectGenre = (genre) => {
+		this.setState({ selectedGenre: genre });
+	};
+
+	render() {
+		return (
+			<center>
+				<h1>Module 1</h1>
+				<Counter initialValue={7} /><br />
+				<SearchForm
+					initialSearchQuery={this.state.searchQuery}
+					onSearch={this.handleSearch}
+				/>
+				<GenreSelect
+					genreList={this.state.genreList}
+					selectedGenre={this.state.selectedGenre}
+					onSelect={this.handleSelectGenre}
+				/>
+				<center>
+					<h3>Search Query: {this.state.searchQuery}</h3>
+					<h3>Selected Genre: {this.state.selectedGenre}</h3>
+				</center>
+			</center>
+		);
+	}
 }
 
 export default App;
